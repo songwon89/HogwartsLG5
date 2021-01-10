16c63793828aaae4d7acdcabd84e51226636e8c6
songwon89 <Tsy19890626!>
2021-01-10 16:25:42 +0800

    selenium练习

--- test_selenium/test_selenium.py
+++ test_selenium/test_selenium.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from test_selenium.base import Base
+from time import sleep
+from selenium import webdriver
 
 
-class TestSelenium(Base):
+class TestSelenium:
+    def setup(self):
+        # browser = os.getenv('browser')
+        # # browser = sys.argv()
+        # if browser == 'firefox':
+        #     self.driver = webdriver.Firefox()
+        # elif browser == 'headless':
+        #     self.driver = webdriver.PhantomJS()
+        # elif browser == 'edge':
+        #     self.driver = webdriver.Edge()
+        # else:
+        #     opt = webdriver.ChromeOptions()
+        #     opt.add_experimental_option('w3c', False)
+        #     self.driver = webdriver.Chrome(chrome_options=opt)
+        self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Edge()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+
+    def teardown(self):
+        self.driver.quit()
+
     def test_selenium(self):
         self.driver.get("https://www.baidu.com/")
+        sleep(3)
