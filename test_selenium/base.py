a2702aa287f0b2346129cbd61ed500b24baa6369
songwon89 <Tsy19890626!>
2021-01-11 00:21:21 +0800

    selenium练习

--- test_selenium/base.py
+++ test_selenium/base.py
@@ -8,19 +8,19 @@ from selenium import webdriver
 
 class Base:
     def setup(self):
-        # browser = os.getenv('browser')
-        # # browser = sys.argv()
-        # if browser == 'firefox':
-        #     self.driver = webdriver.Firefox()
-        # elif browser == 'headless':
-        #     self.driver = webdriver.PhantomJS()
-        # elif browser == 'edge':
-        #     self.driver = webdriver.Edge()
-        # else:
-        #     opt = webdriver.ChromeOptions()
-        #     opt.add_experimental_option('w3c', False)
-        #     self.driver = webdriver.Chrome(chrome_options=opt)
-        self.driver = webdriver.Firefox()
+        browser = os.getenv('browser')
+        # browser = sys.argv()
+        if browser == 'firefox':
+            self.driver = webdriver.Firefox()
+        elif browser == 'headless':
+            self.driver = webdriver.PhantomJS()
+        elif browser == 'edge':
+            self.driver = webdriver.Edge()
+        else:
+            opt = webdriver.ChromeOptions()
+            opt.add_experimental_option('w3c', False)
+            self.driver = webdriver.Chrome(chrome_options=opt)
+        # self.driver = webdriver.Firefox()
         self.driver.maximize_window()
         self.driver.implicitly_wait(5)
 
