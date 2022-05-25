5ec2080d60d8d1e89c45613e3a088b8d748762ca
tiansongyuan <312298464@qq.com>
2022-05-25 20:47:33 +0800

    库版本更新

--- test_pytest/conftest.py
+++ test_pytest/conftest.py
@@ -8,3 +8,10 @@ def connectDB():
     print("连接数据库操作")
     yield
     print("断开数据库连接")
+
+
+# 解决pytest执行结果case标题有中文时显示编码不正确
+def pytest_collection_modifyitems(items):
+    for item in items:
+        item.name = item.name.encode('UTF-8').decode('unicode_escape')
+        item._nodeid = item._nodeid.encode('UTF-8').decode('unicode_escape')
--- test_pytest/test_param_fixture.py
+++ test_pytest/test_param_fixture.py
@@ -3,9 +3,11 @@
 import pytest
 
 
-@pytest.fixture(params=[1, 2, 3], ids=['r1', 'r2', 'r3'])
+# 通过传入request，使用request.param实现参数化
+# ids 实现重命名用例名称
+@pytest.fixture(params=[1, 2, 3], ids=['testcase r1', 'testcase r2', '测试用例 r3'])
 def login1(request):
-    data =request.param
+    data = request.param
     print("获取测试数据")
     return f"测试数据:{data}"
 
